0fe095e1793dbb7f446ce20b3a60e7c0428bf710
Lucas <lucasaristiz789@gmail.com>
2024-09-27 19:29:18 -0500

    FEAT: added guard to request apikey (simple implementation)

---
+++ src/api-key/api-key.guard.ts
@@ -0,0 +1,19 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  constructor(private configService: ConfigService) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers.get('x-api-key');
+    if (apiKey !== this.configService.get('api-key')) {
+      return false;
+    }
+
+    return true;
+  }
+}
--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { BookService } from './book.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { ApiKeyGuard } from 'src/api-key/api-key.guard';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
 
+  @UseGuards(ApiKeyGuard)
   @Post()
   create(@Body() createBookDto: CreateBookDto) {
     return this.bookService.create(createBookDto);
